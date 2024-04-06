f3c96743c2780f8625dded55ca5b63c65394cdc0
mohamed <s6834615@gmail.com>
2024-04-06 14:26:56 +0200

    Add project files.

---
+++ Alien Invasion/Stack.h
@@ -0,0 +1,143 @@
+
+#include <iostream>
+using namespace std;
+#include "Node.h"
+
+template <typename T>
+class LinkedStack
+{
+private:
+	Node<T>* frontPtr;
+public:
+	LinkedStack();
+	bool isEmpty() const;
+	bool Push(const T& newEntry);
+	bool Pop(T& frntEntry);
+	bool peek(T& frntEntry)  const;
+	void PrintStack();
+	~LinkedStack();
+};
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: Satck()
+The constructor of the Stack class.
+
+*/
+
+template <typename T>
+LinkedStack<T>::LinkedStack()
+{
+	frontPtr = nullptr;
+}
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: isEmpty
+Sees whether this Stack is empty.
+
+Input: None.
+Output: True if the Stack is empty; otherwise false.
+*/
+template <typename T>
+bool LinkedStack<T>::isEmpty() const
+{
+	return (frontPtr == nullptr);
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*Function:Push
+Adds newEntry at the back of this Stack.
+
+Input: newEntry .
+Output: True if the operation is successful; otherwise false.
+*/
+
+template <typename T>
+bool LinkedStack<T>::Push(const T& newEntry)
+{
+	Node<T>* newNodePtr = new Node<T>(newEntry);
+	// Insert the new node
+	if (isEmpty())	//special case if this is the first node to insert
+		frontPtr = newNodePtr; // The Stack is empty
+	else{
+		newNodePtr->setNext() = frontPtr; // The Stack was not empty
+		frontPtr = newNodePtr;
+	}
+	return true;
+} 
+
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+/*Function: Pop
+Removes the Last of this Stack. That is, removes the item that was added
+Last.
+
+Input: None.
+Output: True if the operation is successful; otherwise false.
+*/
+
+template <typename T>
+bool LinkedStack<T>::Pop(T& frntEntry)
+{
+	if (isEmpty())
+		return false;
+
+	Node<T>* nodeToDeletePtr = frontPtr;
+	frntEntry = frontPtr->getItem();
+	frontPtr = frontPtr->getNext();
+	// Stack is not empty; remove front
+	
+
+	// Free memory reserved for the deleted node
+	delete nodeToDeletePtr;
+
+	return true;
+}
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+Function: peek
+copies the front of this Stack to the passed param. The operation does not modify the Stack.
+
+Input: None.
+Output: The front of the Stack.
+*/
+
+template <typename T>
+bool LinkedStack<T>::peek(T& frntEntry) const
+{
+	if (isEmpty())
+		return false;
+
+	frntEntry = frontPtr->getItem();
+	return true;
+
+}
+template<typename T>
+void LinkedStack<T>::PrintStack()
+{
+	T K;
+	cout << "\n Stack contents: ";
+	while (this->Pop(K))
+		cout << K << " ";
+	cout << endl;
+}
+///////////////////////////////////////////////////////////////////////////////////
+
+
+template <typename T>
+LinkedStack<T>::~LinkedStack()
+{
+
+	T temp;
+	while (Pop(temp));
+
+}
+
+#endif
---
+++ Alien Invasion/Unit.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+using namespace std;
+#include <iostream>
+class Unit
+{
+private:
+	static int Id;
+	string Type;
+	int Health;
+	int Power;
+	int AttackCapacity;
+public:
+	Unit();
+	void setType(string type) { Type = type; };
+	string getType() const { return Type; };
+
+	static void IncID() { Id += 1; };
+	int getID() const { return Id; };
+
+	void setPower(int Power) { this->Power = Power; };
+	int getPower() const { return Power; };
+
+	void setHealth(int Health) { this->Health = Health; };
+	int getHealth() const { return Health; };
+
+	void setAttackCapacity(int Cap) { this->AttackCapacity = Cap; };
+	int getAttackCapacity() const { return AttackCapacity; };
+
+	virtual bool Attack(Unit K) = 0;
+	
+};
+
